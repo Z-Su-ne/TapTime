30dd13a6556a2ee751b1049c56aa2ff5769cbf35
zou.yp <zou.yp@qq.com>
2025-03-02 00:31:35 +0800

    tdesign导入

--- mobile/src/main.js
+++ mobile/src/main.js
@@ -1,10 +1,14 @@
-import {
-	createSSRApp
-} from "vue";
+import { createSSRApp } from "vue";
 import App from "./App.vue";
+
+// 全量注册TDesign
+import TDesign from "tdesign-mobile-vue";
+import "tdesign-mobile-vue/es/style/index.css";
+
 export function createApp() {
-	const app = createSSRApp(App);
-	return {
-		app,
-	};
+  const app = createSSRApp(App);
+  app.use(TDesign);
+  return {
+    app,
+  };
 }
