7aad5058562a38bf96b529ab254120c282df5357
zou.yp <zou.yp@qq.com>
2025-03-04 00:15:02 +0800

    modified:   mobile/src/App.vue 	modified:   mobile/src/pages/index/index.vue

--- mobile/src/pages/index/index.vue
+++ mobile/src/pages/index/index.vue
@@ -1,58 +1,80 @@
 <template>
-  <!-- 展示层 -->
   <view class="view">
-    <!-- 主页按钮 -->
-    <view class="bottom"></view>
-    <!-- okr show -->
-    <view class="my-okr">
+    <!-- 今日看板 -->
+    <view class="box">
+      <view class="title">今日专注</view>
       <t-cell-group theme="card">
-        <t-cell :left-icon="filter3" title="专注目标" arrow @click="handleClick('AppIcon')" />
-        <t-cell :left-icon="PenQuillIcon" title="所有目标" arrow />
-        <t-cell :left-icon="internet" title="" arrow />
+        <t-cell :left-icon="noticeBoard.icon" :title="'今日专注：' + noticeBoard.title + '分钟'" :note="noticeBoard.note + '次'" />
       </t-cell-group>
     </view>
-    <!-- tab-bar -->
-    <t-tab-bar v-model="value" shape="round" theme="tag" :split="false">
-      <t-tab-bar-item v-for="item in list" :key="item.value" :value="item.value">
-        <template #icon>
-          <t-icon :name="item.icon" />
-        </template>
-      </t-tab-bar-item>
-    </t-tab-bar>
+    <!-- 目标库 -->
+    <view class="box">
+      <view class="title">目标库</view>
+      <t-cell-group theme="card">
+        <t-cell v-for="item in orkShowList" :key="item.value" :left-icon="item.icon" :title="item.title" :note="item.note" arrow />
+      </t-cell-group>
+    </view>
+    <!-- 今日专注日志 -->
+    <view class="box">
+      <view class="title">专注日志</view>
+      <t-cell-group theme="card">
+        <t-cell v-for="item in orkShowList" :key="item.value" :left-icon="item.icon" :title="item.title" arrow />
+      </t-cell-group>
+    </view>
+    <!-- 底部导航栏 -->
+    <view class="box">
+      <t-tab-bar v-model="tabBarSelect" :split="false">
+        <t-tab-bar-item v-for="item in tabBar" :key="item.value" :value="item.value">
+          {{ item.text }}
+          <template #icon>
+            <t-icon :name="item.icon" />
+          </template>
+        </t-tab-bar-item>
+      </t-tab-bar>
+    </view>
   </view>
 </template>
 
 <script>
-import { h } from "vue";
-import { ref } from "vue";
-// import { Filter3Icon, PenQuillIcon, InternetIcon } from "tdesign-icons-vue-next";
+import { h, ref } from "vue";
 import { Icon as TIcon } from "tdesign-icons-vue-next";
 
 export default {
   data() {
     return {
-      Filter3Icon: () => h(Filter3Icon),
-      PenQuillIcon: () => h(PenQuillIcon),
-      internet: () => h(InternetIcon),
-      // const value = ref('label_1');
-      list: [
-        { value: "label_1", label: "首页", icon: "filter-3" },
-        { value: "label_2", label: "应用", icon: "app" },
-        { value: "label_3", label: "聊天", icon: "chat" },
-        { value: "label_4", label: "我的", icon: "user" },
+      // 今日看板
+      noticeBoard: { title: "xx", note: "xx", icon: () => h(TIcon, { name: "time" }) },
+      // 目标库
+      orkShowList: [
+        { value: "show1", title: "专注目标", icon: () => h(TIcon, { name: "filter-3" }), note: 1 },
+        { value: "show2", title: "所有目标", icon: () => h(TIcon, { name: "focus" }), note: 2 },
       ],
+      // 导航栏
+      tabBar: [
+        { value: "tabBar1", text: "主页", icon: "home" },
+        { value: "tabBar2", text: "探索", icon: "system-sum" },
+        { value: "tabBar3", text: "个人", icon: "user" },
+      ],
+      tabBarSelect: "tabBar1",
     };
   },
-  methods: {
-    handleClick(type) {
-      console.log(`点击了${type}项`);
-      // 这里可添加页面跳转逻辑 <button class="citation-flag" data-index="9">
-      uni.navigateTo({
-        url: `/pages/${type}/index`,
-      });
-    },
-  },
+  methods: {},
 };
 </script>
 
-<style></style>
+<style>
+.box {
+  padding: 8px 0;
+  display: block;
+}
+.noticeBoard {
+  margin: 0 16px;
+  background-color: white;
+  border-radius: 12px;
+}
+.title {
+  font-size: large;
+  font-weight: bold;
+  margin: 8px 16px;
+}
+</style>
